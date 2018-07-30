9d536b325ef1f2a675d93ec592a329bdf9177447
S1ealth <s1ealth@me.com>
2018-07-30 18:37:54 +0300

    add modal in test mode

--- www/js/main.js
+++ www/js/main.js
@@ -43,3 +43,20 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " selected";
   });
 }
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// When the user clicks on the button, open the modal 
+btn.addEventListener('click', function(e) {
+  console.log(e.target)
+    modal.style.display = "block";
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+})
